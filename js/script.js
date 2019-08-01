0fdfcef143998988b4017ef8097e31eeb7c60dcf
llow <low.lucyy@gmail.com>
2019-08-01 10:48:19 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	for (i=0; i<24; i++)	{
 	
 		if (i==12) {
-			$('#board').append("<div data-value='1' class='selected freesquare' id='sqfree'><div class='text'><br/>free space</div></div>");
+			$('#board').append("<div data-value='1' class='selected freesquare' id='sqfree'><div class='text'><br/>FREE MICROAGGRESSION</div></div>");
 			$('#board').append("<div data-value='0' class='square' id='sq12'><div class='text'><br/>"+JSONBingo.squares[i].square+"</div></div>");
 		} else {
 			$('#board').append("<div data-value='0' class='square' id='sq"+i+"'><div class='text'><br/>"+JSONBingo.squares[i].square+"</div></div>");
