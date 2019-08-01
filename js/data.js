d8296c4519acd7df08ffdb2fda6881f792ac5f31
llow <low.lucyy@gmail.com>
2019-08-01 10:08:56 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -5,40 +5,48 @@ var footerText = "<a href='https://github.com/lucylow/Microaggression_bingo' tar
 
 var winText = "Winner Winner Privilege Dinner";
 
+var introSnd = new Audio("audio/lets%20play%20the%20privledge%20game%20intro.mp3");
+
+var clickSnd = new Audio("audio/click.mp3");
+
 var clickSnd = new Audio("audio/click.mp3");
 
 var winSnd = new Audio("audio/win.mp3");
 
+var privledgeSnd = new Audio("audio/privilege.mp3");
+
+
+
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
+        {"square": "I am white."},
+        {"square": "I am a man."},
+        {"square": "My name is James, John, or Robert."},
+        {"square": "I have never felt unsafe because of my gender."},
+        {"square": "I have never been catcalled."},
+        {"square": "I have never been raped."},
+        {"square": "I have never been asked 'So where are you really from?'. "},
+        {"square": "I have never been asked 'Why don't you have an accent?'."},
+        {"square": "I was not bullied as a child for any of my identities. :("},
+        {"square": "My parents pay all of my bills."},
+        {"square": "I do not rely on public transportation."},
+        {"square": "I have never been a victim of violence because of my race."},
+        {"square": "I never had to 'come out'."},
+        {"square": "I have never been called fag/dyke/fairy or any other derogatory slur for homosexuals."},
+        {"square": "I do not have any learning disabilities."},
+        {"square": "I do not have any physical disabilities."},
+        {"square": "I have never been shamed for my body type."},
+        {"square": "I can afford a therapist."},
+        {"square": "My name is Michael, William or David."},
+        {"square": "I am not nervous in airport secuity lines."},
+        {"square": "You have never heard this statement: 'You have been randomly selected for secondary passport control'."},
+        {"square": "I am Christian."},
+        {"square": "I have never been the only person of my race in a room."},
+        {"square": "I have never been told I am attractive 'for my race.'"},    	
+        {"square": "I feel comfortable in the gender I was born as."},    	
+        {"square": "I make more money than my professional counterparts of a different gender."},    	
+        {"square": "I have never gone to bed hungry."}, 
+        {"square": "My parents are heterosexual."},    	
+        {"square": "My name is Richard, Joseph, or Chis."},    	
+        {"square": "There is a place of worship for my religion in my town."}    	
     ]
 };
