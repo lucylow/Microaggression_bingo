c03d30b0fe42030232100f0cd1d67bf0e8c49429
(^˵◕ω◕˵^) <low.lucyy@gmail.com>
2019-08-12 03:35:44 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,3 +1,21 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '411268966406901',
+      xfbml      : true,
+      version    : 'v4.0'
+    });
+
+    // ADD ADDITIONAL FACEBOOK CODE HERE
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
 
 var headerText = "Microaggression Bingo";
 
